540bb93d7b106db9fcd8d06f53db401355495aed
Aizaz Jamil <aizaz-jamil28@outlook.com>
2019-07-22 20:28:19 +0500

    about

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// (function ($) {
+//     var tl = new TimelineLite();
+//     tl
+//     // .from('.lg-one', 0.75, {attr:{x1:0, x2:1000}, y: 15, autoAlpha: 0, ease:Power4.easeInOut})
+//     // .from('.lg-two', 0.75, {attr:{x1:0, x2:1000}, y: 15, autoAlpha: 0, ease:Power4.easeInOut})
+//     // .from('.lg-three', 0.75, {attr:{x1:0, x2:1000}, y: 15, autoAlpha: 0, ease:Power4.easeInOut})
+//     // .from('.lg-two', 0.3, {y: 15, autoAlpha: 0, ease: Elastic.easeOut.config(1, 0.3)})
+//     // .from('.lg-three', 0.3, {y: 15, autoAlpha: 0, ease: Elastic.easeOut.config(1, 0.3)})
+//     .staggerFrom('.hiddentext', 1.5, {y:"100%", ease:Power4.easeOut}, 0.15)
+
+// })(jQuery);
+
+// tl = new TimelineMax();
+
+// tl
+//   .to(".text h2", 0.3, { opacity: 1 })
+
+//   .staggerFrom(".large", 1.5, { y: "100%", ease: Power4.easeOut }, 0.15);
+
+// document.querySelector("button").addEventListener("click", letsAnimate);
+
+// function letsAnimate() {
+//   tl.play();
+// }
--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-function resizeNav(){$("#nav-fullscreen").css({"height":window.innerHeight});var radius=Math.sqrt(Math.pow(window.innerHeight,2)+Math.pow(window.innerWidth,2));var diameter=radius*2;$("#nav-overlay").width(diameter);$("#nav-overlay").height(diameter);$("#nav-overlay").css({"margin-top":-radius,"margin-left":-radius})}
-$(document).ready(function(){$("#nav-toggle").click(function(){$("#nav-toggle, #nav-overlay, #nav-fullscreen").toggleClass("open")});$(window).resize(resizeNav);resizeNav()})
-
-AOS.init();
