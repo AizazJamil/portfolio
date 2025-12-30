157c6c9b9914c1f2ba65ce19cd96224c7de3ca6f
Aizaz Jamil <aizaz-jamil28@outlook.com>
2025-12-31 04:13:09 +0500

    Revert "Updated Build"
    This reverts commit e3a3565c3be6da49c37074e433bdd747f73e1fb6.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./App.scss";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
-import SmoothScroll from "./components/Scroll/SmoothScroll";
-function App() {
-    return (_jsx(SmoothScroll, { children: _jsx(BrowserRouter, { children: _jsx(AppRoutes, {}) }) }));
-}
-export default App;
--- src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Link } from "react-router-dom";
-const Button = ({ children, link, target }) => {
-    return (_jsx("div", { className: "button-wrap", children: _jsxs(Link, { to: link, className: "button btn__primary", target: target, rel: target === "_blank" ? "noopener noreferrer" : undefined, children: [_jsx("span", { className: "dot" }), " ", children] }) }));
-};
-export default Button;
